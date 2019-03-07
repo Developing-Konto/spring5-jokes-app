e82d244275c10490b0aebbc37fee560d6208f599
Butcheer <developing.konto@gmail.com>
2019-03-07 18:58:21 +0100

    Java Class Configuration Example

---
+++ src/main/java/com/butcheer/joke/config/ChuckConfiguration.java
@@ -0,0 +1,17 @@
+package com.butcheer.joke.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Butcheer on 2019-03-07 18:54
+ */
+@Configuration
+public class ChuckConfiguration {
+
+   @Bean
+   public ChuckNorrisQuotes chuckNorrisQuotes() {
+      return new ChuckNorrisQuotes();
+   }
+}
